e1b9e1e24f027eefac6fcc4b5601de86dc185fdf
Yen-ting Cho <antonytaro@gmail.com>
2025-08-15 16:56:49 +0800

    fix bug cksum

--- clean.bash
+++ clean.bash
@@ -2,4 +2,6 @@
 ip netns del send 2>/dev/null
 ip netns del recv 2>/dev/null
 umount /sys/fs/bpf 2>/dev/null || true
-rm -rf /sys/fs/bpf/tc /sys/fs/bpf/atu_rx 
+rm -rf /sys/fs/bpf/tc
+rm -rf /sys/fs/bpf/atu_tx
+rm -rf /sys/fs/bpf/atu_rx
